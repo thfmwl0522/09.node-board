e5091535701433bbdb46d7fd5311ee0ad177830e
thfmwl0522 <thfmwl0522@naver.com>
2020-12-01 18:27:42 +0900

    vue

--- routes/book.js
+++ routes/book.js
@@ -86,9 +86,10 @@ router.post('/save', isUser, upload.single('upfile'), async (req, res, next) => 
 		}
 		else {
 			// 파일을 올리지 않았거나, 올렸거나
+			req.body.uid = req.session.user ? req.session.user.id : null;
 			let rs = await sqlGen('books', 'I', {
-				field: ['title', 'writer', 'content', 'wdate'], 
-				data: req.body, 
+				field: ['title', 'writer', 'content', 'wdate', 'uid'],
+				data: req.body,
 				file: req.file
 			});
 			res.redirect('/book/list');
